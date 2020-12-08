53ca2e08f18d32530d415b95b161f3e885dc15a4
whyiamavirgin <izmajlovtima@gmail.com>
2020-12-08 07:32:38 +0600

    ratatata

--- app3.js
+++ app3.js
@@ -5,6 +5,61 @@ const age = 17
 
 const output = 'Привет, меня зовут ' + name + ' и мой возраст ' + age + ' лет.'
 const outputNew = `Привет, меня зовут ${name} и мой возраст ${age} лет.` // Удобный и новый способ 
+const ternOutput = `Привет, меня зовут ${name} и мой возраст ${age < 30 ? 'A' : 'B'} лет`
 
 console.log(output)
 console.log(outputNew)
+console.log(ternOutput)
+
+const output2 = `
+    <div>This is div</div>
+    <p>This is p</p>
+
+`
+// * ``
+// TODO Такие кавычки очень удобные и сохраняют все пробелы, отступы и переносы строк
+
+console.log(output2)
+
+// ! Методы
+
+const name2 = 'Timofey'
+console.log(name2.length)
+console.log(name2.toUpperCase())
+console.log(name2.toLowerCase())
+console.log(name2.charAt(2))
+console.log(name2.indexOf('fey'))
+console.log(name2.indexOf('aaaaaaaaaa')) // когда метод indexOf() не находит в строке заданный параметр, выводится "-1"
+console.log(name2.startsWith('Tim')) // true/false
+console.log(name2.endsWith('ffff')) // true/false
+console.log(name2.toLowerCase().startsWith('Tim')) // можно ообэединять методы
+console.log(name2.repeat(3)) // повторяет текст заданное кол-во раз
+
+const string = '           password             '
+// все пробелы сохраняются
+// чтобы очистить пробелы, используется метод 'trim()'
+console.log(string.trim())
+console.log(string.trimLeft()) // очищает пробелы только слева
+console.log(string.trimRight()) // очищает пробелы только справа
+
+function logPerson(s, name3, age2) {
+    console.log(s, name3, age2)
+    if (age2 < 0 ) {
+        age2 = 'Еще не родился'
+    }
+    return `${s[0]}${name3}${s[1]}${age2}${s[2]}`
+}
+
+const personName = 'Timofey'
+const personAge = 17
+const personAge2 = -17
+const personName2 = 'Maksim'
+
+// новый синтаксис передачи параметров в функцию
+const output4 = logPerson`Имя: ${personName}, Возраст: ${personAge} !`
+const output5 = logPerson`Имя: ${personName2}, Возраст: ${personAge2} !`
+
+console.log(output4)
+console.log(output5)
+
+// ! 4 Часть - app4.js
---
+++ app4.js
@@ -0,0 +1,92 @@
+// ! 4 Часть - Функции
+
+
+// * Function Expression
+// Когда функцию складываем в переменную 
+
+const greet2 = function greet2(name) {
+    console.log('Привет2 - ', name)
+}
+
+greet('Lena')
+greet2('Lena')
+
+// ! Различия между FD и FE
+// К FD мы можем обращаться до инициализации самой функции, а в случае с FE, выводится ошибка
+
+// * Function Declaration
+// Обычная, всемилюбимая функция
+
+function greet(name) {
+    console.log('Привет - ', name)
+}
+
+
+console.log(typeof greet) // В JS нет типа данных "function" // ! Функции являются объектами
+console.dir(greet)
+
+// ! Анонимные функции
+
+let counter = 0
+const interval = setInterval(function() { // Есть еще setTimeout
+    if (counter === 1) {
+        clearInterval(interval) // clearTimeout()
+    } else {
+        console.log(++counter)
+    }
+}, 1000)
+
+// ! Стрелочные функции
+
+function greet3(name) {
+    console.log('Привет - ', name)
+}
+
+const arrow = (name) =>  {
+    console.log('Привет - ', name)
+}
+
+const arrow2 = name => {console.log('Привет - ', name)}
+
+arrow('Tim')
+arrow2('Timofey')
+
+const pow2 = num => num ** 2 // Возводит в степень // ! ЭТО ТОЖЕ ФУНКЦИЯ
+
+console.log(pow2(5))
+
+// ! Параметры по умолчанию
+
+const sum = (a = 40, b = a * 2) => a + b
+
+console.log(sum(41))
+console.log(sum())
+console.log(sum(10, 5))
+
+function sumAll(...all) { // все числа записываются в массив 'all'. неограниченное кол-во чисел
+    console.log(all)
+    let result = 0
+    for (let num2 of all) {
+        result += num2
+    }
+    return result
+}
+
+const res = sumAll(1,2,3,4,5)
+console.log(res)
+
+// ! Замыкания 
+
+function createMember(name) {
+    return function(lastName) {
+        console.log(name + lastName)
+    }
+}
+
+const logWithLastName = createMember('Timofey')
+// console.log(logWithLastName)
+console.log(logWithLastName('Adamiya'))
+console.log(logWithLastName('Izmailov'))
+
+// ! Конец 
+// Следующий урок - массивы
